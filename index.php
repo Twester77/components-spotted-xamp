ec42daa23e743fba510cd1333ed9616a425b00ea
Leonardo F <87826@unifev.edu.br>
2026-03-16 14:53:27 -0300

    criado login.php e adicionado ao index

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <?php include 'components/navbar.php'; ?>
 
 <main style="max-width: 800px; margin: auto; padding: 20px;">
+    <?php include 'components/login.php'; ?>
     <article style="text-align: center;">
         <h2 style="font-size: 22px; text-align: center; margin-bottom: 20px;"> Bem vindos "A Fenda" (e não, não é do bíquini). A sua Comunidade Universitária da UNIFEV! </h2>
         <img src="imagensfoto/Capa Entrada.jpg" alt="Capa Home do Site" style="width: 60%; border-radius: 10px; margin: 20px 0;">
---
+++ login.php
@@ -0,0 +1,35 @@
+
+
+<form action="action_page.php" method="post">
+    <div class="imgcontainer"> 
+      <img src="imagensfoto/img_avatar2.jpg" alt="Avatar" class="avatar" >
+      <img src="imagensfoto/img_avatar1.jpg" alt="Avatar" class="avatar" >
+    </div>
+
+    <div class="container">
+        <div class="input-group">
+            <label for="uname"><b>Usuário</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+        </div>
+
+        <div class="input-group">
+            <label for="psw"><b>Senha</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+        </div>
+
+    <div style="margin: 15px 0; display: flex; align-items: center; gap: 8px;">
+      <input type="checkbox" name="terms" id="terms" required>
+      <label for="terms" style="font-size: 13px; color: #ddc80e; font-weight: bold;">
+        Eu aceito as <a href="diretrizes.php" style="color: #ddc80e; text-decoration: underline;">Diretrizes de Segurança e Termo de Responsabilidade</a>
+     </label>
+   </div>
+
+        <button type="submit" style="background-color: #4CAF50; color: white; padding: 10px; width: 100%; cursor: pointer;">
+            Login
+        </button>
+    
+        <button type="button" onclick="deslogar()" class="cancelbtn" style="color: #f44336; margin-top: 10px; width: 100%; color: white; padding: 10px; cursor: pointer;">
+           Sair da Conta
+        </button>
+    </div> 
+ </form>
