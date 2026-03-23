4b15d542a1f9e0878b5f7636ddd68e763c0c627b
Leonardo F <87826@unifev.edu.br>
2026-03-23 17:25:40 -0300

    esqueci de incluir header e navbar

--- novo-post.php
+++ novo-post.php
@@ -1,22 +1,17 @@
 <?php
 include 'conexao.php';
-session_start();
+include 'components/header.php'; 
+include 'components/navbar.php'; 
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <title>Postar na Fenda</title>
-</head>
-<body>
 
-<div class="form-container">
-    <h2>🚀 Lançar na Fenda</h2>
+<div class="form-wrapper"> <div class="form-container">
+    <h2> Enviar Novo Post </h2>
     <form action="enviar-post.php" method="POST">
         <textarea name="mensagem" placeholder="O que tá rolando na UNIFEV?" required maxlength="800"></textarea>
         
         <label for="categoria">Categoria:</label>
+
         <select name="categoria" id="categoria">
             <option value="anonimo">🕵️ Anônimo </option>
             <option value="comunidade">👥 Comunidade / Evento </option>
@@ -25,12 +20,10 @@ session_start();
             <option value="ranco"> 👌 Ranço </option>
             <option value="desabafo"> 😩 Acaba pelo amor de Deus </option>
 
-        
-
         </select>
 
-        <button type="lancar"> Lançar ao Mar!</button>
-        <a href="feed.php" style="display:block; text-align:center; color:#666; text-decoration:none;">Voltar ao Feed</a>
+        <button type="submit"> Lançar ao Mar!</button>
+        <a href="feed.php" style="display:block; text-align:center; color:#666; text-decoration:none;"> Voltar ao Feed</a>
     </form>
 </div>
 
