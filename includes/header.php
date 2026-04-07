8898efbdf2607643ef488f1132a3ab90b63fc357
Leonardo F <87826@unifev.edu.br~>
2026-04-06 21:02:43 -0300

    branch do pc na unifev, depois mexer em estilos

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,7 @@ $usuario_logado = isset($_SESSION['usuario_id']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>A Fenda - Spotted Universitário (Votuporanga)</title>
-    <link rel="stylesheet" href="/spotted-unifev/estilos.css">    
+    <link rel="stylesheet" href="estilos.css">    
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
